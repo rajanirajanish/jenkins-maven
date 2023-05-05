8baefd5be23f93a8c2441bf9474f43c298c78abb
rajanish <120563062+rajanirajanish@users.noreply.github.com>
2023-05-05 10:22:31 +0530

    Update App.java

--- src/main/java/jenkins/App.java
+++ src/main/java/jenkins/App.java
@@ -1,13 +1,14 @@
 package jenkins;
 
 /**
- * Hello world!
- *
+ * Hi Murali sir!
+ *This is from Rajanish!
  */
 public class App 
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hi Murali sir!" );
+         System.out.println("This is from Rajanish!)
     }
 }
